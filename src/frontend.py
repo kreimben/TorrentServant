8ccb94b5842d2eaf4bf5df0a51bbf6dd22ceb855
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 08:10:00 +0900

    Add current time showing feature and reload button.

--- src/frontend.py
+++ src/frontend.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
@@ -14,7 +16,8 @@ def ready_for_frontend() -> FastAPI:
         context = {
             'request': request,
             'title': 'TorrentServant',
-            'content': 'Hello! This is TorrentServant.'
+            'content': 'Hello! This is TorrentServant.',
+            'currentTime': datetime.datetime.now()
         }
         return templates.TemplateResponse('index.html', context)
 
