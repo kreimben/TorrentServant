cc4f3c52113a858f837bc92ef916b9519c1aae54
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 09:23:29 +0900

    Change `mount` method to `add_route` method.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import uvicorn as uvicorn
 import ready
 import frontend
+from router import storage
 
 app = ready.get_application()
 
-app.mount('/', frontend.ready_for_frontend())
+app.add_route('/', frontend.router)
 # TODO: mount backend logics.
+# app.mount('/router/storage', storage.storage)
+app.add_route('/api/storage', storage.router)
 
 if __name__ == '__main__':
-    uvicorn.run(app='main:app', host='0.0.0.0', port='8000')
+    uvicorn.run(app, host='0.0.0.0', port='8000')
