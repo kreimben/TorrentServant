4685f214e495add5b7d4b8a72ba98e859348065d
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 19:00:35 +0900

    Add swagger responses.

--- src/router/storage.py
+++ src/router/storage.py
@@ -9,25 +9,69 @@ router = APIRouter(
 )
 
 
-@router.get('/space', description='k, m, g, t, p, e. Default is \'g\' for GigaBytes.')
+@router.get(
+    '/space',
+    description='k, m, g, t, p, e. Default is \'g\' for GigaBytes.',
+    responses={
+        200: {
+            'content': {
+                'application/json': {
+                    'examples': {
+                        'normal response': {
+                            'summary': 'Success',
+                            'value': {
+                                'total': '1GB',
+                                'used': '0.5GB',
+                                'free': '0.5GB'
+                            }
+                        }
+                    }
+                }
+            },
+            'description': 'Get left usage disk space.',
+        },
+        400: {
+            'content': {
+                'application/json': {
+                    'examples': {
+                        'error': {
+                            'summary': 'Fatal Error',
+                            'value': {
+                                'message': 'error message from python\'s os error.'
+                            }
+                        }
+                    }
+                }
+            },
+            'description': 'When \`os.getcwd()\` function is not working'
+        }
+    })
 async def space(request: Request, option: str = 'g'):
-    cwd = os.getcwd()
-    usage = shutil.disk_usage(cwd + '/../Downloads')
+    response = {}
 
-    total_size = _byte_transform(usage.total, option)
-    used_size = _byte_transform(usage.used, option)
-    free_size = _byte_transform(usage.free, option)
+    try:
+        cwd = os.getcwd()
+        usage = shutil.disk_usage(cwd + '/../Downloads')
 
-    return {'total': total_size, 'used': used_size, 'free': free_size}
+    except Exception as e:
+        return {'message': str(e)}
+
+    finally:
+        total_size = _byte_transform(usage.total, option)
+        used_size = _byte_transform(usage.used, option)
+        free_size = _byte_transform(usage.free, option)
+
+        response = {'total': total_size, 'used': used_size, 'free': free_size}
+        return response
 
 
 def _byte_transform(bytes: int, to: str, bsize=1024) -> str:
-    a = {'k' : 1, 'm': 2, 'g' : 3, 't' : 4, 'p' : 5, 'e' : 6 }
+    a = {'k': 1, 'm': 2, 'g': 3, 't': 4, 'p': 5, 'e': 6}
     r = float(bytes)
     for i in range(a[to]):
         r = r / bsize
 
-    results = str(round(r,2))
+    results = str(round(r, 2))
 
     if to == 'k':
         results += 'KB'
