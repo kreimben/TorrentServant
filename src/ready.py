20169cc7210b82376e7f3265ee8128a940d9a527
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 09:22:54 +0900

    Refactoring

--- src/ready.py
+++ src/ready.py
@@ -1,9 +1,11 @@
 import fastapi.applications
 from fastapi import FastAPI
 import event
+import environments
+
 
 def get_application() -> fastapi.applications.FastAPI:
-    app = FastAPI()
+    app = FastAPI(name=environments.APP_NAME, version=environments.APP_VERSION, debug=True)
     event.ready_for_event(app)
 
     return app
