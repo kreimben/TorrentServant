c305305b7f16b44d55af5b7116eff7c87a4df2fe
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 09:23:59 +0900

    Adjust router (APIRouter).

--- src/frontend.py
+++ src/frontend.py
@@ -1,24 +1,23 @@
 import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
+router = APIRouter(
+    tags=['storage'],
+)
 
-def ready_for_frontend() -> FastAPI:
-    app = FastAPI()
+templates = Jinja2Templates(directory="../templates")
 
-    templates = Jinja2Templates(directory="../templates")
 
-    @app.get("/", response_class=HTMLResponse)
-    async def root(request: Request):
-        context = {
-            'request': request,
-            'title': 'TorrentServant',
-            'content': 'Hello! This is TorrentServant.',
-            'currentTime': datetime.datetime.now()
-        }
-        return templates.TemplateResponse('index.html', context)
-
-    return app
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    context = {
+        'request': request,
+        'title': 'TorrentServant',
+        'content': 'Hello! This is TorrentServant.',
+        'currentTime': datetime.datetime.now()
+    }
+    return templates.TemplateResponse('index.html', context)
