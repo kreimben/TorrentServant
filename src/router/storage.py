c20f29bf8fd4a37817ced469db32e6f6a840b846
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-09 18:11:44 +0900

    Implement rest disk usage space feature.

--- src/router/storage.py
+++ src/router/storage.py
@@ -4,15 +4,47 @@ import shutil
 import os
 
 router = APIRouter(
-    prefix='/storage',
+    prefix='/api/storage',
     tags=['storage'],
 )
 
 
-@router.get('/space')
-async def space(request: Request):
-    downloads_dir = os.getcwd()
-    print('downloads_dir: {}'.format(downloads_dir))
-    usage = shutil.disk_usage('../../Downloads')
+@router.get('/space', description='k, m, g, t, p, e. Default is \'g\' for GigaBytes.')
+async def space(request: Request, option: str = 'g'):
+    cwd = os.getcwd()
+    usage = shutil.disk_usage(cwd + '/../Downloads')
 
-    return {'success': True, 'total': usage.total, 'used': usage.used, 'free': usage.free}
+    total_size = _byte_transform(usage.total, option)
+    used_size = _byte_transform(usage.used, option)
+    free_size = _byte_transform(usage.free, option)
+
+    return {'success': True, 'total': total_size, 'used': used_size, 'free': free_size}
+
+
+def _byte_transform(bytes: int, to: str, bsize=1024) -> str:
+    a = {'k' : 1, 'm': 2, 'g' : 3, 't' : 4, 'p' : 5, 'e' : 6 }
+    r = float(bytes)
+    for i in range(a[to]):
+        r = r / bsize
+
+    results = str(round(r,2))
+
+    if to == 'k':
+        results += 'KB'
+
+    elif to == 'm':
+        results += 'MB'
+
+    elif to == 'g':
+        results += 'GB'
+
+    elif to == 't':
+        results += 'TB'
+
+    elif to == 'p':
+        results += 'PB'
+
+    elif to == 'e':
+        results += 'EB'
+
+    return results
