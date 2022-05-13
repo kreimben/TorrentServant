f25a24d8f3a1b2f15666e5358199843411cdd1db
Aksidion Kreimben <aksidion@kreimben.com>
2022-05-13 16:47:51 +0900

    Remove redundant file.

--- src/router/torrent.py
+++
@@ -1,8 +0,0 @@
-import libtorrent as lt
-
-
-class Torrent:
-    def __init__(self):
-        self.session = lt.session({'listen_interfaces': '0.0.0.0:6881'})
-        print('session: {}'.format(self.session))
-        lt
